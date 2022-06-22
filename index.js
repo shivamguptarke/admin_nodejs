6f350580bb8ec1e14c80ac3d736f97350b24e739
shivamguptarke <shivamguptarke@gmail.com>
2022-06-22 19:56:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/testAdminDB');
+
+
+app.listen(3000, console.log("server is up and running at 3000"));
+
+app.get("/", (req, res) => {
+    res.redirect("index.html");
+})
